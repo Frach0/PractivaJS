23b29728d1f303a3c8474cc7fbafa714b1c0d3c6
Santiago <fsantiagoa2000@gmail.com>
2022-04-10 22:08:00 -0300

    Agrego la solucion al ejercicio

---
+++ Modulo01/Ejercicio01.js
@@ -0,0 +1,10 @@
+//Crea una variable llamada carName, asignale el valor Volvo a ella.
+
+//Este ejercicio puede tener multiples respuestas
+
+var carName = "Volvo"
+
+// let carName = "Volvo"; let le da un ambito local y su valor tambien puede ser modificado.
+// const  carName = "Volvo"; es de ambito local al igual que let y su valor es constante no se puede modificar.
+
+console.log(carName);
